c0d9c54be07024d6bd8a78d58853c59a40e13728
Bor333 <mugen999999999@gmail.com>
2024-04-17 16:40:37 +0700

    auth form with condition

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php
-
-$x = 3;
-$y = 5;
-$y = $y ^ $x = $x ^ $y = $x ^ $y;
-//65
-
-echo "x = $x и y = $y";
-
+$login = "admin";
+$pass = "123";
+$auth = false;
+if ($login == "admin" && $pass == "123") {
+    $user = $login;
+    $auth = true;
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php if ($auth): ?>
+Добро пожаловать <?=$user?>!
+<?php  else: ?>
+<form action="">
+    <input type="text">
+    <input type="password">
+    <input type="submit" value="Войти">
+</form>
+<?php endif; ?>
+</body>
+</html>
 
 
