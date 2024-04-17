3e9effe7a79563ad57d60a7141eb2f08b3ea0434
Bor333 <mugen999999999@gmail.com>
2024-04-17 07:57:50 +0700

    the third page creating way

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-$age = 25;
-echo <<<START
-    Привет "Alex". 
-    Тебе {$age} лет.
-    <a href="index.php">Далее</a>
-START;
+$title = 'Главная страница - страница обо мне';
+$h1 = 'Информация обо мне';
+$year = 2024;
+$img = 'plane.jpg';
+
+$content = file_get_contents('main.html');
+$content = str_replace('{{ TITLE }}', $title, $content);
+$content = str_replace('{{ H1 }}', $h1, $content);
+$content = str_replace('{{ YEAR }}', $year, $content);
+$content = str_replace('{{ IMG }}', $img, $content);
+echo $content;
+
 
 
