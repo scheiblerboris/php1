10033768e8d5e3e6e218c605502704c278a57a2c
Bor333 <mugen999999999@gmail.com>
2024-04-19 07:07:27 +0700

    render

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 
-function find($start, $history, $target)
-{
-    if ($start == $target)
-        echo "{$target} = {$history}<br>";
-
-    elseif ($start > $target)
-        return NULL;
-    else
-        return find($start + 5, "({$history} + 5)", $target)
-            || find($start * 3, "({$history} * 3)", $target);
-    return NULL;
+function renderTemplate($page, $content = '') {
+    ob_start();
+    include $page . ".php";
+    return ob_get_clean();
 }
 
-find(1, "1", 9);
+$content = renderTemplate('about', 2000);
+echo renderTemplate('layout', $content);
