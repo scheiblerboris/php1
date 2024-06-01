4ba366c0f6aab009b108a4e05f6d87ee0baf4ba1
Bor333 <mugen999999999@gmail.com>
2024-06-01 09:40:13 +0700

    render without engine

---
+++ catalog.php
@@ -0,0 +1,45 @@
+<?php
+$catalog = [
+    'img' => 'img/pizza.jpeg',
+    'name' => 'Пицца',
+    ''
+];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php include 'menu.php' ?>
+
+<h2>Каталог</h2>
+<div>
+    <div>
+        <h2>Пицца</h2>
+        <img src="/img/pizza.jpeg" width="150"><br>
+        Цена: 24<br>
+        <button>Купить</button>
+        <hr>
+    </div>
+    <div>
+        <h2>Чай</h2>
+        <img src="/img/tea.png" width="150"><br>
+        Цена: 1<br>
+        <button>Купить</button>
+        <hr>
+    </div>
+    <div>
+        <h2>Яблоко</h2>
+        <img src="/img/apple.jpg" width="150"><br>
+        Цена: 12<br>
+        <button>Купить</button>
+        <hr>
+    </div>
+</div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,4 @@
+<a href="/">Главная</a>
+<a href="/catalog.php">Каталог</a>
+<a href="/about.php">о нас</a>
+<br>
